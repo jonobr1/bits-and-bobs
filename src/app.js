b81595ade935844343c019d9b65b3f6a4dd24f77
Jono Brandel <developer@jono.fyi>
2025-08-21 11:10:47 -0700

    No camera movement

--- src/app.js
+++ src/app.js
@@ -266,7 +266,6 @@ export default function App(domElement) {
         });
       }
       // camera.position.y -= (window.scrollY * 0.001 + camera.position.y) * drag;
-      camera.position.y = -window.scrollY / 1000;
       group.rotation.y = (-0.01 * elapsed) / 1000;
       composer.render();
       previousElapsed = elapsed;
